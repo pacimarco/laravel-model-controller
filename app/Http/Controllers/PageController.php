9d259c0ed60b88785ba995951adb72ed7e3fc541
pacimarco <m.paci1987@gmail.com>
2022-09-28 17:46:14 +0200

    errore stampa all movie(sqlstate)

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
@@ -11,6 +12,9 @@ class PageController extends Controller
     }
 
     public function list(){
+        $movies = Movie::all();
+        /*foreach($movies as $movie) per stampare altre info*/
+        return view('list', compact('movies'));
         return view('movie_list');
     }
 }
